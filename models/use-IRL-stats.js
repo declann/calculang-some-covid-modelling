bb1e2c7b399e5ce9773d205b3a23ee7789da4435
Declan Naughton <declan.naughton2@mail.dcu.ie>
2022-02-02 23:16:08 +0000

    validation excercise (in excel)

--- models/use-IRL-stats.js
+++ models/use-IRL-stats.js
@@ -10,19 +10,22 @@ for (
   t1 = t0;
   date_fns.isBefore(t1, new Date(2022, 0, 12));
   t1 = date_fns.addDays(t1, 1)
-)
+) {
   out.push({
     t1,
+    new_cases: model.new_cases({ t_in: t1 }),
+    new_deaths: model.new_deaths({ t_in: t1 }),
+    new_cases_smoothed: model.new_cases_smoothed({ t_in: t1 }),
+    new_deaths_smoothed: model.new_deaths_smoothed({ t_in: t1 }),
     cases_deaths_link_smoothed: model.cases_deaths_link_smoothed({ t_in: t1 }),
     cases_deaths_link: model.cases_deaths_link({ t_in: t1 }),
   });
+}
 
-// output a csv
-console.log('date,cases_deaths_link,cases_deaths_link_smoothed');
-out.forEach((d) => {
-  console.log(
-    `${date_fns.format(d.t1, 'yyyy-MM-dd')},${d.cases_deaths_link},${
-      d.cases_deaths_link_smoothed
-    }`
-  );
-});
+  // output a csv
+  console.log('date,cases_deaths_link,cases_deaths_link_smoothed,new_cases,new_deaths,new_cases_smoothed,new_deaths_smoothed');
+  out.forEach((d) => {
+    console.log(
+      `${date_fns.format(d.t1, 'yyyy-MM-dd')},${d.cases_deaths_link},${d.cases_deaths_link_smoothed},${d.new_cases},${d.new_deaths},${d.new_cases_smoothed},${d.new_deaths_smoothed}`
+    );
+  });
