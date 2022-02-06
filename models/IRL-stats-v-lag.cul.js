30139b4dbdc64b5bebce1a0f30e44d154eb08c16
Declan Naughton <declan.naughton2@mail.dcu.ie>
2022-02-06 22:56:53 +0000

    include data_date_extent in model-of-model, this is why i need all_cul...

--- models/IRL-stats-v-lag.cul.js
+++ models/IRL-stats-v-lag.cul.js
@@ -1,4 +1,4 @@
-import { new_cases_smoothed, new_deaths_smoothed, new_cases, new_deaths, cases_deaths_link_smoothed, cases_deaths_link } from './IRL-stats.cul.js';
-export { new_cases_smoothed, new_deaths_smoothed, new_cases, new_deaths, cases_deaths_link_smoothed, cases_deaths_link };
+import { new_cases_smoothed, new_deaths_smoothed, new_cases, new_deaths, cases_deaths_link_smoothed, cases_deaths_link, data_date_extent } from './IRL-stats.cul.js';
+export { new_cases_smoothed, new_deaths_smoothed, new_cases, new_deaths, cases_deaths_link_smoothed, cases_deaths_link, data_date_extent };
 
 export const lag = () => (lag_in); // in the model-of-model lag for the base model is now an input
