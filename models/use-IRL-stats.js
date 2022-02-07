8ab09f11f4279200a600b1d9729049ecbcc01b9b
Declan Naughton <declan.naughton2@mail.dcu.ie>
2022-02-07 12:24:29 +0000

    prune csv for direct load to vega

--- models/use-IRL-stats.js
+++ models/use-IRL-stats.js
@@ -1,31 +1,32 @@
 var model = require('./IRL-stats.js');
 var date_fns = require('date-fns');
 
-var t0 = new Date(2020, 2, 17);
-var t1;
+var t0 = new Date(2020, 3, 17);
+var t_in;
 
 var out = [];
 
 for (
-  t1 = t0;
-  date_fns.isBefore(t1, model.data_date_extent({})) || date_fns.isEqual(t1, model.data_date_extent({}));
-  t1 = date_fns.addDays(t1, 1)
+  t_in = t0;
+  date_fns.isBefore(t_in, model.data_date_extent({})) || date_fns.isEqual(t_in, model.data_date_extent({}));
+  t_in = date_fns.addDays(t_in, 1)
 ) {
   out.push({
-    t1,
-    new_cases: model.new_cases({ t_in: t1 }),
-    new_deaths: model.new_deaths({ t_in: t1 }),
-    new_cases_smoothed: model.new_cases_smoothed({ t_in: t1 }),
-    new_deaths_smoothed: model.new_deaths_smoothed({ t_in: t1 }),
-    cases_deaths_link_smoothed: model.cases_deaths_link_smoothed({ t_in: t1 }),
-    cases_deaths_link: model.cases_deaths_link({ t_in: t1 }),
+    date: t_in,
+    // optimising for size : manually. Arquero? (csv wired to browser)
+    //new_cases: model.new_cases({ t_in: t1 }),
+    //new_deaths: model.new_deaths({ t_in: t1 }),
+    cases_deaths_link_smoothed: model.cases_deaths_link_smoothed({t_in}) == Infinity ? null : +(+model.cases_deaths_link_smoothed({t_in})).toFixed(2), // vega doesn't like Infinity!
+    cases_smoothed: +(+model.new_cases_smoothed({t_in})).toFixed(2),
+    deaths_smoothed: +(+model.new_deaths_smoothed({t_in})).toFixed(2)
+      //cases_deaths_link: model.cases_deaths_link({ t_in: t1 }),
   });
 }
 
   // output a csv
-  console.log('date,cases_deaths_link,cases_deaths_link_smoothed,new_cases,new_deaths,new_cases_smoothed,new_deaths_smoothed');
+  console.log('date,cases_deaths_link_smoothed,cases_smoothed,deaths_smoothed');
   out.forEach((d) => {
     console.log(
-      `${date_fns.format(d.t1, 'yyyy-MM-dd')},${d.cases_deaths_link},${d.cases_deaths_link_smoothed},${d.new_cases},${d.new_deaths},${d.new_cases_smoothed},${d.new_deaths_smoothed}`
+      `${date_fns.format(d.date, 'yyyy-MM-dd')},${d.cases_deaths_link_smoothed},${d.cases_smoothed},${d.deaths_smoothed}`
     );
   });
